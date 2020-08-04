e255cbec384a4d7e5bf562ba6f9e11628c7c5b95
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-08-04 23:32:25 +0200

    Shorten the links by using rel.link api

--- src/components/Shortener/Shortener.tsx
+++ src/components/Shortener/Shortener.tsx
@@ -1,19 +1,127 @@
-import React, { FC } from 'react';
-import { Wrapper, Container, SrLabel, Input, Button } from './style';
+import React, { FC, useState, useRef } from 'react';
+import axios from 'axios';
+import { validateUrl } from '../../utils/validateUrl';
+import {
+  Section,
+  Wrapper,
+  Form,
+  SrLabel,
+  Input,
+  ErrorMsg,
+  Button,
+  Results,
+  ListItem,
+  OriginalLink,
+  ShortenLink,
+} from './style';
+
+interface Link {
+  original: string;
+  shortened: string;
+}
+
+interface Error {
+  isError: boolean | null;
+  msg: string | null;
+}
+
+type Submit = React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement, MouseEvent>;
 
 export const Shortener: FC = () => {
+  const [query, setQuery] = useState<string>('');
+  const [results, setResults] = useState<Link[]>([]);
+  const [error, setError] = useState<Error>({ isError: null, msg: null });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setError({ isError: null, msg: null });
+    setQuery(e.target.value);
+  };
+
+  const copyToClipboard = (idx: number): void => {
+    // find shorten link hash based on index
+    const copiedLink = `https://rel.ink/${results[idx].shortened}`;
+    navigator.clipboard.writeText(copiedLink);
+  };
+
+  const handleSubmit = async (event: Submit): Promise<void> => {
+    event.preventDefault();
+    // prevent submiting empty input
+    if (!query) {
+      setError({ isError: true, msg: 'Please add a link' });
+    } else {
+      // check if input string is correct url
+      if (validateUrl(query)) {
+        try {
+          let response = await axios.post('https://rel.ink/api/links/', {
+            url: query,
+          });
+
+          // display freshest results on top
+          // limit results to 3
+          if (results.length < 3) {
+            setResults([{ original: query, shortened: response.data.hashid }, ...results]);
+          } else {
+            setResults([
+              { original: query, shortened: response.data.hashid },
+              results[0],
+              results[1],
+            ]);
+          }
+
+          // clear input after each api call
+          setQuery('');
+        } catch (error) {
+          alert('Something went wrong...');
+        }
+      } else {
+        // when input does not contain correct url
+        setError({ isError: true, msg: 'Please add a valid link' });
+      }
+
+      return;
+    }
+  };
+
   return (
-    <Wrapper>
-      <Container>
-        <SrLabel htmlFor="inputLink">Paste a link to shorten it:</SrLabel>
-        <Input
-          id="inputLink"
-          type="text"
-          placeholder="Shorten a link here..."
-          autoComplete="inputLink"
-        />
-        <Button type="submit">Shorten It!</Button>
-      </Container>
-    </Wrapper>
+    <Section>
+      <Wrapper>
+        <Form onSubmit={(e) => handleSubmit(e)}>
+          <SrLabel htmlFor="inputLink">Paste a link to shorten it:</SrLabel>
+          <div>
+            <Input
+              value={query}
+              id="inputLink"
+              name="inputLink"
+              type="text"
+              placeholder="Shorten a link here..."
+              onChange={(e) => handleChange(e)}
+              onBlur={(e) => handleChange(e)}
+              error={error.isError}
+              aria-invalid={error.isError ? 'true' : 'false'}
+            />
+            {error.isError && <ErrorMsg role="alert">{error.msg}</ErrorMsg>}
+          </div>
+          <Button type="submit" onClick={(e) => handleSubmit(e)}>
+            Shorten It!
+          </Button>
+        </Form>
+        {results && (
+          <Results>
+            <SrLabel as="span">Shortened links</SrLabel>
+            <ul>
+              {results.map((result, idx) => (
+                <ListItem key={idx}>
+                  <OriginalLink href={result.original}>{result.original}</OriginalLink>
+                  <ShortenLink href={`https://rel.ink/${result.shortened}`}>
+                    https://rel.ink/{result.shortened}
+                  </ShortenLink>
+                  <button onClick={() => copyToClipboard(idx)}>Copy</button>
+                </ListItem>
+              ))}
+            </ul>
+          </Results>
+        )}
+      </Wrapper>
+    </Section>
   );
 };
--- src/components/Shortener/style.ts
+++ src/components/Shortener/style.ts
@@ -3,25 +3,61 @@ import { theme } from '../../theme/mainTheme';
 import bgMobile from '../../assets/images/bg-shorten-mobile.svg';
 import bgDesktop from '../../assets/images/bg-shorten-desktop.svg';
 
-export const Wrapper = styled.div`
-  margin: 0 auto;
-  max-width: 111rem;
+export const Section = styled.div`
+  position: relative;
+  padding-left: 2.4rem;
+  padding-right: 2.4rem;
+
+  &:after {
+    content: '';
+    background-color: ${theme.colors.gray};
+    position: absolute;
+    top: 8rem;
+    left: 0;
+    right: 0;
+    height: 100%;
+    z-index: -1;
+  }
+
+  @media (min-width: 775px) {
+    &:after {
+      top: 6rem;
+    }
+  }
+
+  @media (min-width: 965px) {
+    &:after {
+      top: 9rem;
+    }
+  }
 `;
 
-export const Container = styled.div`
+export const Wrapper = styled.div`
+  max-width: 111rem;
+  margin: 0 auto;
+`;
+
+export const Form = styled.form`
   display: flex;
   position: relative;
   z-index: 10;
   flex-direction: column;
+  max-width: 111rem;
   margin-top: 9rem;
-  margin-left: 2.4rem;
-  margin-right: 2.4rem;
+  margin-left: auto;
+  margin-right: auto;
   padding: 2.6rem;
   border-radius: 1rem;
   background-color: ${theme.colors.background};
   background-image: url(${bgMobile});
   background-position: top right;
   background-repeat: no-repeat;
+
+  & > div {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+  }
 
   @media (min-width: 775px) {
     flex-direction: row;
@@ -47,27 +83,49 @@ export const SrLabel = styled.label`
   width: 1px;
 `;
 
-export const Input = styled.input`
-  border: 3px solid ${theme.colors.white};
+interface InputProps {
+  error: boolean | null;
+}
+
+export const Input = styled.input<InputProps>`
+  border: 3px solid ${(props) => (props.error ? theme.colors.error : theme.colors.white)};
+  color: ${(props) => (props.error ? theme.colors.error : theme.colors.black)};
   border-radius: 0.5rem;
   outline: none;
-  padding: 0.8rem 1.3rem;
+  padding: 1.3rem;
   font-family: inherit;
-  font-size: 1.6rem;
+  font-size: 1.5rem;
   font-weight: ${theme.fontWeight.regular};
 
   ::placeholder {
-    color: ${theme.colors.gray300};
+    color: ${(props) => (props.error ? theme.colors.error : theme.colors.gray300)};
   }
 
   @media (min-width: 775px) {
-    flex-grow: 1;
     padding-top: 1.2rem;
     padding-bottom: 1.2rem;
     padding-left: 3rem;
     border-radius: 1rem;
     margin-right: 2rem;
     font-size: 2rem;
+  }
+`;
+
+export const ErrorMsg = styled.p`
+  color: ${theme.colors.error};
+  font-size: 1.3rem;
+  font-style: italic;
+  margin-top: 0.8rem;
+  padding-left: 0.5rem;
+
+  @media (min-width: 775px) {
+    position: absolute;
+    top: 8.4rem;
+  }
+
+  @media (min-width: 965px) {
+    top: 12rem;
+    font-size: 1.6rem;
   }
 `;
 
@@ -93,5 +151,82 @@ export const Button = styled.button`
     font-size: 2rem;
     border-radius: 1rem;
     padding: 1.4rem 4rem;
+  }
+`;
+
+export const Results = styled.div`
+  position: relative;
+  z-index: 12;
+  overflow: hidden;
+`;
+
+export const ListItem = styled.li`
+  margin-top: 3rem;
+  background-color: ${theme.colors.white};
+  display: flex;
+  flex-direction: column;
+  padding: 2rem;
+  border-radius: 0.8rem;
+  white-space: nowrap;
+  text-align: left;
+
+  & > button {
+    border: none;
+    background-color: ${theme.colors.primary};
+    font-family: inherit;
+    font-size: 1.6rem;
+    font-weight: ${theme.fontWeight.bold};
+    color: ${theme.colors.white};
+    border-radius: 0.5rem;
+    padding: 0.8rem 3rem;
+    cursor: pointer;
+    transition: ease-in-out 350ms background-color;
+    &:hover {
+      background-color: ${theme.colors.primaryLight};
+    }
+  }
+
+  @media (min-width: 775px) {
+    margin-top: 2rem;
+    flex-direction: row;
+    align-items: center;
+
+    & > button {
+      font-size: 1.5rem;
+      padding: 1rem 3rem;
+    }
+  }
+`;
+
+export const OriginalLink = styled.a`
+  flex: 1;
+  color: ${theme.colors.gray300};
+  border-bottom: 2px solid ${theme.colors.gray};
+  padding-bottom: 1.6rem;
+  text-decoration: none;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  font-size: 1.6rem;
+
+  @media (min-width: 775px) {
+    font-size: 2rem;
+    line-height: 3rem;
+    margin-right: 2.6rem;
+    border-bottom: none;
+    padding-bottom: 0;
+  }
+`;
+
+export const ShortenLink = styled.a`
+  text-decoration: none;
+  color: ${theme.colors.primary};
+  margin-top: 1.6rem;
+  margin-bottom: 1.6rem;
+  margin-right: 2rem;
+
+  @media (min-width: 775px) {
+    margin-top: 0;
+    margin-bottom: 0;
   }
 `;
