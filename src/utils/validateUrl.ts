9058c25841aa6b9d5f65868af57487fe8907301b
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-08-04 23:30:46 +0200

    Add helper fn to validate urls

---
+++ src/utils/validateUrl.ts
@@ -0,0 +1,15 @@
+type URL = string;
+
+export const validateUrl = (url: URL): boolean => {
+  const regEx = new RegExp(
+    '^(https?:\\/\\/)?' +
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+      '((\\d{1,3}\\.){3}\\d{1,3}))' +
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+      '(\\?[;&a-z\\d%_.~+=-]*)?' +
+      '(\\#[-a-z\\d_]*)?$',
+    'i',
+  );
+
+  return !!regEx.test(url);
+};
